95a1bea6693b5fbaa179e5b46107946b6435bca6
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-19 20:50:26 +0300

    Red test of RedMushrooms project

---
+++ JuniorMind/RedMushrooms/RedMushroomsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RedMushrooms
+{
+    [TestClass]
+    public class RedMushroomsTests
+    {
+        [TestMethod]
+        public void ThreeMushrooms()
+        {
+            int redMushrooms = CalculateNumberOfRedMushrooms(3, 2);
+            Assert.AreEqual(2, redMushrooms);
+        }
+
+        int CalculateNumberOfRedMushrooms(int totalNumberOfMushrooms, int multiplierOfRedMushrooms)
+        {
+            return 0;
+        }
+    }
+}
