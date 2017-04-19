8d4bbb58e64b76d324e94c1619ddf2560ad9e59f
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-19 06:52:35 +0300

    Add empty HayEatenByGoat project

---
+++ JuniorMind/HayEatenByGoat/HayEatenByGoatTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HayEatenByGoat
+{
+    [TestClass]
+    public class HayEatenByGoatTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ JuniorMind/HayEatenByGoat/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HayEatenByGoat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HayEatenByGoat")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("30ac7ba6-1770-411a-a3b9-3415259ca4d0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
