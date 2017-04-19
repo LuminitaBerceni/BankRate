26f29b74a156a867fdca0f225dd56c09d49b0972
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-19 21:09:08 +0300

    Add empty RepetitionsPerformedBySportsman project

---
+++ JuniorMind/RepetitionsPerformedBySportsman/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RepetitionsPerformedBySportsman")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RepetitionsPerformedBySportsman")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a7244d6c-4566-4ba9-be8d-8d6d0210fdc5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
