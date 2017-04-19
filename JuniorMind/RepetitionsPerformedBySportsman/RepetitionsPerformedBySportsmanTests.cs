c5c984d2804552e483fdd3ed59fb9128c878a673
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-19 21:19:01 +0300

    Red Test

---
+++ JuniorMind/RepetitionsPerformedBySportsman/RepetitionsPerformedBySportsmanTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepetitionsPerformedBySportsman
+{
+    [TestClass]
+    public class RepetitionsPerformedBySportsmanTests
+    {
+        [TestMethod]
+        public void RepetitionsForTwoRounds()
+        {
+            int repetitions = CalculateRepetitionsPerformedBySportsman(2);
+            Assert.AreEqual(4, repetitions);
+        }
+
+        int CalculateRepetitionsPerformedBySportsman(int numberOfRoundAndRepetitions)
+        {
+            return 0;
+        }
+    }
+}
