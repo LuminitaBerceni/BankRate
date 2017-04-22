c2ac03d66aad8fea5def9978c23df2b629ccfc6e
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-22 15:57:41 +0300

    Calculate number of parquet(2X3) for a room (5X5)

--- JuniorMind/Parquet/ParquetTests.cs
+++ JuniorMind/Parquet/ParquetTests.cs
@@ -13,9 +13,18 @@ namespace Parquet
             Assert.AreEqual(4, parquet);
         }
 
+        [TestMethod]
+        public void ParquetFor5X5Room()
+        {
+            double parquet = CalculateNrParquet(5, 5, 2, 3);
+            Assert.AreEqual(5, parquet);
+        }
+
         double CalculateNrParquet(double roomWidth, double roomLenght, double parquetWidth, double parquetLenght)
         {
-            double parquetPieces = (roomWidth * roomLenght)/(parquetWidth*parquetLenght);
+            double roomDimension = roomWidth * roomLenght;
+            double parquetDimension = parquetWidth * parquetLenght;
+            double parquetPieces = roomDimension/parquetDimension;
             double parquetLoss = 0.15 * Math.Ceiling(parquetPieces);
             return Math.Ceiling(parquetPieces + parquetLoss);
         }
