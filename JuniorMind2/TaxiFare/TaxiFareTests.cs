83adb8f0e5959616602a907dec00a06e82342c1c
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-24 23:44:25 +0300

    Calculate nighttime taxi fare for medium distance

--- JuniorMind2/TaxiFare/TaxiFareTests.cs
+++ JuniorMind2/TaxiFare/TaxiFareTests.cs
@@ -30,6 +30,12 @@ namespace TaxiFare
             Assert.AreEqual(7, CalculateTaxiFare(1, 21));
         }
 
+        [TestMethod]
+        public void NighttimeFareForMediumDistances()
+        {
+            Assert.AreEqual(210, CalculateTaxiFare(21, 21));
+        }
+
         decimal CalculateTaxiFare(int distanceInKm, int hour)
         {
             decimal pricePerKm = GetPricePerKm(distanceInKm, GetPrices(hour));
@@ -39,7 +45,7 @@ namespace TaxiFare
         decimal [] GetPrices (int hour)
         {
             decimal[] daytimePrices = { 5, 8, 6 };
-            decimal[] nighttimePrices = { 7 };
+            decimal[] nighttimePrices = { 7 ,10};
             return IsDayTime(hour) ? daytimePrices : nighttimePrices;
         }
 
