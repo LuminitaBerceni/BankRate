a03bcbd2d472b4c986478dbff3d172de7e5f6ffd
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-24 06:42:21 +0300

    Add empty DistanceFlyedByBird project

---
+++ JuniorMind/DistanceFlyedByBird/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DistanceFlyedByBird")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DistanceFlyedByBird")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("66d44f8b-b2c4-45b9-a63e-5f21b5449d5b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
