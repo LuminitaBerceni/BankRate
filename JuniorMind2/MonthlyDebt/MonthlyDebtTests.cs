c67e9adb8c3e3aae06f056f6a75f4f0cacefe185
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-25 07:00:44 +0300

    Calculate total debt for over thirty delay days

--- JuniorMind2/MonthlyDebt/MonthlyDebtTests.cs
+++ JuniorMind2/MonthlyDebt/MonthlyDebtTests.cs
@@ -18,11 +18,26 @@ namespace MonthlyDebt
             Assert.AreEqual(155, CalculateTotalDebt(100, 11));
         }
 
+        [TestMethod]
+        public void TotalDebtForOverThirtyDelayDays()
+        {
+            Assert.AreEqual(410, CalculateTotalDebt(100, 31));
+        }
+
         decimal CalculateTotalDebt (decimal monthRent, int delayDays)
         {
-            decimal penaltiPerDay = delayDays > 10 ? (decimal) 5/100 : (decimal) 2 / 100;
+            decimal penaltiPerDay = GetPenaltiPerDay(delayDays);
             decimal penalties = delayDays * monthRent * penaltiPerDay;
             return monthRent + penalties;
+        }
+
+        private static decimal GetPenaltiPerDay(int delayDays)
+        {
+            if (delayDays < 10)
+                return (decimal) 2 / 100;
+            if (delayDays > 10 && delayDays < 30)
+                return (decimal) 5 / 100;
+            return (decimal) 10 / 100;
         }
     }
 }
