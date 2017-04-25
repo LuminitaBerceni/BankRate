c569ed7defc0d5422bc658447849a97827e74cfc
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-25 06:22:58 +0300

    Add empty MonthlyDebt project

---
+++ JuniorMind2/MonthlyDebt/MonthlyDebtTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MonthlyDebt
+{
+    [TestClass]
+    public class MonthlyDebtTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
