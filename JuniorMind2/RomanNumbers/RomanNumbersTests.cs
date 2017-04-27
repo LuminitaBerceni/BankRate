89f8036baa65ea734c17379694faaeec8ec96f5e
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-27 22:01:26 +0300

    Convert number smaller than fifty

--- JuniorMind2/RomanNumbers/RomanNumbersTests.cs
+++ JuniorMind2/RomanNumbers/RomanNumbersTests.cs
@@ -13,10 +13,23 @@ namespace RomanNumbers
             Assert.AreEqual("V", romanNumber);
         }
 
+        [TestMethod]
+        public void ConvertNumberSmallerThanFifty()
+        {
+            string romanNumber = ConvertNumberInRomanNumber(26);
+            Assert.AreEqual("XXVI", romanNumber);
+        }
+
         string ConvertNumberInRomanNumber(int number)
         {
             string[] romanNumbersSmallerThanTen = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
-            return romanNumbersSmallerThanTen[number-1];
+            string[] romanNumbersUpToOneHundred = { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C" };
+            if (number < 10)
+                return romanNumbersSmallerThanTen[number - 1];
+            if (number >= 10)
+                return romanNumbersUpToOneHundred[(number / 10) - 1] + romanNumbersSmallerThanTen[(number % 10) - 1];
+            else
+                return "Numarul este mai mare decat 100!";
         }
 
     }
