b4aa622e5ab1efbbb4208c17407674def28a9f94
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-27 22:06:23 +0300

    Display a message if the number is greater than one hundred

--- JuniorMind2/RomanNumbers/RomanNumbersTests.cs
+++ JuniorMind2/RomanNumbers/RomanNumbersTests.cs
@@ -27,13 +27,20 @@ namespace RomanNumbers
             Assert.AreEqual("LXXXIX", romanNumber);
         }
 
+        [TestMethod]
+        public void ConvertNumberGreaterThanOneHundred()
+        {
+            string romanNumber = ConvertNumberInRomanNumber(101);
+            Assert.AreEqual("Numarul este mai mare decat 100!", romanNumber);
+        }
+
         string ConvertNumberInRomanNumber(int number)
         {
             string[] romanNumbersSmallerThanTen = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
             string[] romanNumbersUpToOneHundred = { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C" };
             if (number < 10)
                 return romanNumbersSmallerThanTen[number - 1];
-            if (number >= 10)
+            if (number >= 10 && number <=100)
                 return romanNumbersUpToOneHundred[(number / 10) - 1] + romanNumbersSmallerThanTen[(number % 10) - 1];
             else
                 return "Numarul este mai mare decat 100!";
