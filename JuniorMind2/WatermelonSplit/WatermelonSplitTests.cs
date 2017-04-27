3fb7d9de17dc8f4d3c1c933e7362d6f544c24ea0
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-27 22:37:02 +0300

    Watermelon can't be split in two even parts

--- JuniorMind2/WatermelonSplit/WatermelonSplitTests.cs
+++ JuniorMind2/WatermelonSplit/WatermelonSplitTests.cs
@@ -12,6 +12,12 @@ namespace WatermelonSplit
             Assert.AreEqual("DA",CanSplitWatermelonInTwoEvenParts(10));
         }
 
+        [TestMethod]
+        public void CantSplitWatermelon()
+        {
+            Assert.AreEqual("NU", CanSplitWatermelonInTwoEvenParts(5));
+        }
+
         string CanSplitWatermelonInTwoEvenParts(int watermelonWeight)
         {
             return (watermelonWeight % 2 == 0 && watermelonWeight >= 4) ? "DA" : "NU";
