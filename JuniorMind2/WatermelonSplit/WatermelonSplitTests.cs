518934ea3af911c679aac600c54bf148446c0c68
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-04-27 22:23:09 +0300

    Add empty WatermelonSplit project

---
+++ JuniorMind2/WatermelonSplit/WatermelonSplitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WatermelonSplit
+{
+    [TestClass]
+    public class WatermelonSplitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
