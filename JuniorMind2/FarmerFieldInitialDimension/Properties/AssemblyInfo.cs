8d19fcc207cf74c5456945995b3fd20351b6e5f2
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-05-01 23:19:47 +0300

    Add empty FarmerFieldInitialDimension project

---
+++ JuniorMind2/FarmerFieldInitialDimension/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FarmerFieldInitialDimension")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FarmerFieldInitialDimension")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5faf5619-2349-426b-b8a7-1f26e219612f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
