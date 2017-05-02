c7183914344cd58a11b13bf6286b2e6874da3133
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-05-02 07:20:14 +0300

    Calculate number of squares in eight by eight chess table

--- JuniorMind3/SquaresInChessTable/SquaresInChessTableTests.cs
+++ JuniorMind3/SquaresInChessTable/SquaresInChessTableTests.cs
@@ -13,11 +13,18 @@ namespace SquaresInChessTable
             Assert.AreEqual(1, squares);
         }
 
+        [TestMethod]
+        public void SquaresOnEightByEightTable()
+        {
+            int squares = CalculateSquaresInChessTable(8);
+            Assert.AreEqual(204, squares);
+        }
+
         int CalculateSquaresInChessTable (int number)
         {
             int totalSquares = 0;
             for (int i = 1; i <= number; i++)
-                totalSquares = totalSquares + (i * i);
+                totalSquares += i * i;
             return totalSquares;
         }
     }
