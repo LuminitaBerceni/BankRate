5019ca8ff832b219488ee40e1de55e13375c2202
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-05-02 07:03:54 +0300

    Add empty SquaresInChessTable project

---
+++ JuniorMind3/SquaresInChessTable/SquaresInChessTableTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SquaresInChessTable
+{
+    [TestClass]
+    public class SquaresInChessTableTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
