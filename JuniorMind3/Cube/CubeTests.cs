a3e51bb55d53c2c8328fdcac129e62bfeb59dc35
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-05-04 06:45:44 +0300

    Red test for Cube project

--- JuniorMind3/Cube/CubeTests.cs
+++ JuniorMind3/Cube/CubeTests.cs
@@ -7,8 +7,15 @@ namespace Cube
     public class CubeTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetFirstCube()
         {
+            int number = GetCube(1);
+            Assert.AreEqual(192, number);
+        }
+
+        int GetCube(int k)
+        {
+            return 0;
         }
     }
 }
