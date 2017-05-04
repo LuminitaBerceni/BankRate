d38f16481aefa84db7b029b36fa8539533fd3454
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-05-04 06:21:57 +0300

    Check if the alphabet is a pangram

--- JuniorMind3/Pangram/PangramTests.cs
+++ JuniorMind3/Pangram/PangramTests.cs
@@ -10,12 +10,23 @@ namespace Pangram
         public void CheckAlphabet()
         {
             string text = CheckIfIsPangram("abcdefghijklmnopqrstuvwxyz");
-            Assert.AreEqual("Yes", text);
+            Assert.AreEqual("Yes", text );
         }
 
         string CheckIfIsPangram(string phrase)
         {
-            return null;
+            for (int j = 'a'; j <= 'z'; j++)
+            {
+                int count = 0;
+                for (int i = 0; i < phrase.Length; i++)
+                {
+                    if (phrase[i] == j)
+                        count++;
+                }
+                if (count == 0)
+                    return "No";
+            }
+            return "Yes";
         }
     }
 }
