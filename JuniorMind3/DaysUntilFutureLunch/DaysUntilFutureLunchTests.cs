354c027a1c9ebd6031fffef9c727d8ecc3a07297
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-05-07 21:12:24 +0300

    Add empty DaysUntilFutureLunch project

---
+++ JuniorMind3/DaysUntilFutureLunch/DaysUntilFutureLunchTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DaysUntilFutureLunch
+{
+    [TestClass]
+    public class DaysUntilFutureLunchTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ JuniorMind3/DaysUntilFutureLunch/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DaysUntilFutureLunch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DaysUntilFutureLunch")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3639a7ef-e2b2-4003-a392-496ef9d90c7a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
