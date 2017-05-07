237901211fbaf28311fe71bc52c3efc1bdd0d9fc
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-05-07 20:31:17 +0300

    Calculate number of anagrams from a word with two identical letters

--- JuniorMind3/Anagrams/AnagramsTests.cs
+++ JuniorMind3/Anagrams/AnagramsTests.cs
@@ -11,21 +11,43 @@ namespace Anagrams
         {
             int anagrams = CalculateAnagrams("ab");
             Assert.AreEqual(2 , anagrams);
+        }
 
+        [TestMethod]
+        public void CalculateAnagramsWithTwoIdenticalLetters()
+        {
+            int anagrams = CalculateAnagrams("aa");
+            Assert.AreEqual(1, anagrams);
         }
 
         int CalculateAnagrams (string word)
         {
-            return CalculateFactorial(word.Length);
+            int factorialForRepetition = 1;
+            for (int i = 'a'; i < 'z'; i++)
+            {
+                factorialForRepetition *= CalculateFactorial(CalculateNumberOfRepetition(word, (char)i));
+            }
+
+            return CalculateFactorial(word.Length) / factorialForRepetition;
         }
 
         int CalculateFactorial(int number)
         {
-            if (number == 1)
-                return 1;
-            else
-                return number * CalculateFactorial(number - 1);
+            int factorial = 1;
+            for (int i = 2; i <= number; i++)
+                factorial *= i;
+            return factorial;
+        }
 
+        int CalculateNumberOfRepetition(string word, char letter)
+        {
+            int counter = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (letter == word[i])
+                    counter++;
+            }
+            return counter;
         }
 
     }
