bc641a824bcf47fc3fd5b2b3a727474f5a9e146f
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-05-14 23:01:50 +0300

    Add empty LottoChances project

---
+++ JuniorMind3/LottoChances/LottoChancesTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LottoChances
+{
+    [TestClass]
+    public class LottoChancesTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
