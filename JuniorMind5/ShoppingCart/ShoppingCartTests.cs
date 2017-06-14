c51304bf469d2a424f89dc46b4e351611721d6bf
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-06-14 07:06:47 +0300

    Find cheapest product

--- JuniorMind5/ShoppingCart/ShoppingCartTests.cs
+++ JuniorMind5/ShoppingCart/ShoppingCartTests.cs
@@ -13,6 +13,13 @@ namespace ShoppingCart
             Assert.AreEqual((decimal)6.25, CalculateTotalPayment(products));
         }
 
+        [TestMethod]
+        public void CheapestProductTest()
+        {
+            var products = new Product[] { new Product("Book", (decimal)2.5), new Product("CD", (decimal)1.5), new Product("Mug", (decimal)0.75), new Product("Frame", (decimal)1), new Product("Magnet", (decimal)0.5) };
+            Assert.AreEqual(products[4], FindCheapestProduct(products));
+        }
+
         struct Product
         {
             public string name;
@@ -33,6 +40,18 @@ namespace ShoppingCart
                 totalPayment += products[i].price;
             }
             return totalPayment;
+        }
+
+        Product FindCheapestProduct(Product[] products)
+        {
+            decimal cheapestProduct = products[0].price;
+            int productIndex = 0;
+            for (int i = 1; i < products.Length; i++)
+            {
+                if (products[i].price < cheapestProduct)
+                    productIndex = i;
+            }
+            return products[productIndex];
         }
     }
 }
