86736635c9594aa350373ff85a0b4fba20a366e4
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-06-16 07:25:07 +0300

    Add GetDirections function in data structure

--- JuniorMind5/Intersection/IntersectionTests.cs
+++ JuniorMind5/Intersection/IntersectionTests.cs
@@ -15,11 +15,21 @@ namespace Intersection
         {
             public int x;
             public int y;
+
             public Point(int x, int y)
             {
                 this.x = x;
                 this.y = y;
             }
+
+            public void GetDirections(Directions direction)
+            {
+                if (direction == Directions.up) y++;
+                if (direction == Directions.down) y--;
+                if (direction == Directions.right) x++;
+                if (direction == Directions.left) x--;
+            }
+
         }
 
         enum Directions
@@ -29,5 +39,7 @@ namespace Intersection
             right,
             left
         }
+
+        
     }
 }
