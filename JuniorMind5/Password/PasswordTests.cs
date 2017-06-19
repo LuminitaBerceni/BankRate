0f78aa055d83c4c2e85f681cbe2b61c2d74aa414
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-06-19 23:04:54 +0300

    Add empty Password project

---
+++ JuniorMind5/Password/PasswordTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Password
+{
+    [TestClass]
+    public class PasswordTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
