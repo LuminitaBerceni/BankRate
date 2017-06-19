5a891a1385a19b33734b6f84a46ed3a056c579cb
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-06-19 23:29:54 +0300

    Add data structure Password

--- JuniorMind5/Password/PasswordTests.cs
+++ JuniorMind5/Password/PasswordTests.cs
@@ -10,5 +10,26 @@ namespace Password
         public void TestMethod1()
         {
         }
+
+        struct Password
+        {
+            public int passwordLength;
+            public int smallLetters;
+            public int capitalLetters;
+            public int digits;
+            public int symbols;
+            public bool exludeSimilarCharacters;
+            public bool excludeAmbiguousCharacters;
+            public Password(int passwordLength, int smallLetters, int capitalLetters, int digits, int symbols, bool exludeSimilarCharacters, bool excludeAmbiguousCharacters)
+            {
+                this.passwordLength = passwordLength;
+                this.smallLetters = smallLetters;
+                this.capitalLetters = capitalLetters;
+                this.digits = digits;
+                this.symbols = symbols;
+                this.exludeSimilarCharacters = exludeSimilarCharacters;
+                this.excludeAmbiguousCharacters = excludeAmbiguousCharacters;
+            }
+        }
     }
 }
