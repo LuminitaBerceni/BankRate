eebfd4d7ee110110774d34c34fb24f7ac0f7bd06
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-06-21 07:54:42 +0300

    Add data structures Cyclist and Cyclometer

--- JuniorMind5/Cyclometer/CyclometerTests.cs
+++ JuniorMind5/Cyclometer/CyclometerTests.cs
@@ -10,5 +10,31 @@ namespace Cyclometer
         public void TestMethod1()
         {
         }
+
+        struct Cyclist
+        {
+            public string name;
+            public double wheelDiameter;
+            public Cyclometer[] records;
+
+            public Cyclist(string name, double wheelDiameter, Cyclometer[] records)
+            {
+                this.name = name;
+                this.wheelDiameter = wheelDiameter;
+                this.records = records;
+            }
+        }
+
+        struct Cyclometer
+        {
+            public int rotations;
+            public int seconds;
+
+            public Cyclometer(string rotations, double seconds)
+            {
+                this.rotations = rotations;
+                this.seconds = seconds;
+            }
+        }
     }
 }
