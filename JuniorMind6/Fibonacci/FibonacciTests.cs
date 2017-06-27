45bbf218835722cc407d89b5d23992e32afd70f3
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-06-27 07:03:18 +0300

    Red test

--- JuniorMind6/Fibonacci/FibonacciTests.cs
+++ JuniorMind6/Fibonacci/FibonacciTests.cs
@@ -7,8 +7,14 @@ namespace Fibonacci
     public class FibonacciTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FibonacciTest1()
         {
+            Assert.AreEqual(1, Fibonacci(1));
+        }
+
+        int Fibonacci(int n)
+        {
+            return 0;
         }
     }
 }
