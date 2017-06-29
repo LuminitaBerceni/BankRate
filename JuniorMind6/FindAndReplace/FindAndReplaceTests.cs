e9b461ed9338dbbb9ab01074bffb4020d88d3b44
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-06-29 07:21:04 +0300

    Find character x and replaces with b

--- JuniorMind6/FindAndReplace/FindAndReplaceTests.cs
+++ JuniorMind6/FindAndReplace/FindAndReplaceTests.cs
@@ -14,7 +14,14 @@ namespace FindAndReplace
 
         string FindAndReplace(string initialString, char replacedCharacter, string replaceString)
         {
-            return "";
+            if(initialString.Length > 0)
+            {
+                string finalString = FindAndReplace(initialString.Substring(1, initialString.Length - 1), replacedCharacter, replaceString);
+                if (initialString[0] == replacedCharacter)
+                    return replaceString + finalString;
+                return initialString[0] + finalString;
+            }
+            return initialString;
         }
     }
 }
