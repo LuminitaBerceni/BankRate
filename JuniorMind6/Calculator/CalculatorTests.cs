34d9be4444ac0daa22cfc8a74340ab44b08ccae4
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-07-03 07:16:29 +0300

    Calculate expression 3*4

--- JuniorMind6/Calculator/CalculatorTests.cs
+++ JuniorMind6/Calculator/CalculatorTests.cs
@@ -9,12 +9,27 @@ namespace Calculator
         [TestMethod]
         public void CalculateExpressionWithOneOperator()
         {
-            Assert.AreEqual(12 , CalculateExpression("* 3 4"));
+            int index = 0;
+            Assert.AreEqual(12 , CalculateExpression("* 3 4", ref index));
         }
 
-        double CalculateExpression(string expression)
+        public double CalculateExpression(string expression, ref int index)
         {
-            return 0;
+            string[] elements = expression.Split(' ');
+            string currentElement = elements[index++];
+            double number;
+            if (Double.TryParse(currentElement, out number))
+            {
+                return number;
+            }
+
+            switch (currentElement)
+            {
+                case "+": return CalculateExpression(expression, ref index) + CalculateExpression(expression, ref index);
+                case "-": return CalculateExpression(expression, ref index) - CalculateExpression(expression, ref index);
+                case "*": return CalculateExpression(expression, ref index) * CalculateExpression(expression, ref index);
+                default: return CalculateExpression(expression, ref index) / CalculateExpression(expression, ref index);
+            }
         }
     }
 }
