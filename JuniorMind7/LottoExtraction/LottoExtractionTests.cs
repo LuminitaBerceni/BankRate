08fedab0015d92debdc848a131fc49568912994d
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-07-26 07:59:26 +0300

    Red test for LottoExtraction project

--- JuniorMind7/LottoExtraction/LottoExtractionTests.cs
+++ JuniorMind7/LottoExtraction/LottoExtractionTests.cs
@@ -7,8 +7,15 @@ namespace LottoExtraction
     public class LottoExtractionTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SortNumbers()
         {
+            int[] lottoNumbers = new int[] {10, 4, 28, 31, 49, 3};
+            CollectionAssert.AreEqual(new int[] { 3, 4, 10, 28, 31, 49 }, SortLottoNumbers(lottoNumbers));
+        }
+
+        int[] SortLottoNumbers(int[] numbers)
+        {
+            return new int[6];
         }
     }
 }
