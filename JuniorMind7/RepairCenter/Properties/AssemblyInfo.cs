636fc969fb2a92ae336e77cb15eebaac8a4e29d0
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-07-30 20:29:54 +0300

    Add empty RepairCenter project

---
+++ JuniorMind7/RepairCenter/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RepairCenter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RepairCenter")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dad27532-b772-45d8-a354-d6fe8085f786")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JuniorMind7/RepairCenter/RepairCenterTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RepairCenter
+{
+    [TestClass]
+    public class RepairCenterTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
