f3a34f5707c02b018c6cc44977ec32368e9ef801
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-07-31 06:50:32 +0300

    Sort orders by priority

--- JuniorMind7/RepairCenter/RepairCenterTests.cs
+++ JuniorMind7/RepairCenter/RepairCenterTests.cs
@@ -7,29 +7,90 @@ namespace RepairCenter
     public class RepairCenterTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SortTest()
         {
+            var orders = new Order[] {
+                new Order(3, "HardDiskRepair",Priority.High),
+                new Order(1, "VentilationSystemCleaning", Priority.Low),
+                new Order(2, "DataRecovery", Priority.High),
+                new Order(4, "DisplayReplacement", Priority.Medium)};
+
+            var sortedOrders = new Order[] {
+                new Order(2, "DataRecovery", Priority.High),
+                new Order(3, "HardDiskRepair",Priority.High),
+                new Order(4, "DisplayReplacement", Priority.Medium),
+                new Order(1, "VentilationSystemCleaning", Priority.Low)};
+
+            CollectionAssert.AreEqual(sortedOrders, SortOrdersByPriority(orders));
         }
 
-        enum Priority
+        public enum Priority
         {
             Low = 1,
             Medium = 2,
             High = 3
         }
 
-        struct Order
+        public struct Order
         {
             public int orderNumber;
             public string orderIssue;
             public Priority priority;
 
-            public Order(int orderNumber, string orderIssue, Priority priority)
+            public Order (int orderNumber, string orderIssue, Priority priority)
             {
                 this.orderNumber = orderNumber;
                 this.orderIssue = orderIssue;
                 this.priority = priority;
             }
+        }
+
+        Order[] SortOrdersByPriority(Order[] orders)
+        {
+            QuickSort(orders, 0, orders.Length - 1);
+            return orders;
+        }
+
+        void QuickSort(Order[] orders, int left, int right)
+        {
+            if (right > left)
+            {
+                var pivot = orders[right];
+                int i = left;
+                int j = right;
+                int index = left;
+                while (index <= j)
+                {
+                    if (FindLowest(pivot, orders[index]))
+                    {
+                        ChangeElementsBetweenThem(ref orders[i++], ref orders[index++]);
+                    }
+                    else if (FindLowest(orders[index], pivot))
+                    {
+                        ChangeElementsBetweenThem(ref orders[index], ref orders[j--]);
+                    }
+                    else
+                    {
+                        index++;
+                    }
+                }
+                QuickSort(orders, left, i - 1);
+                QuickSort(orders, j + 1, right);
+            }   
+        }
+
+        private static bool FindLowest(Order first, Order second)
+        {
+            if (first.priority == second.priority)
+                return first.orderNumber > second.orderNumber;
+            return first.priority < second.priority;
+        }
+
+        private void ChangeElementsBetweenThem(ref Order firstOrder, ref Order secondOrder)
+        {
+            var temp = firstOrder;
+            firstOrder = secondOrder;
+            secondOrder = temp;
         }
     }
 }
