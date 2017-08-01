29941610841e6689f229a6399fd7e157189508c1
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-08-01 06:09:01 +0300

    Add empty WordsSort project

---
+++ JuniorMind7/WordsSort/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WordsSort")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WordsSort")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cf6e1034-b604-4432-9957-838ce0959a8f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ JuniorMind7/WordsSort/WordsSortTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordsSort
+{
+    [TestClass]
+    public class WordsSortTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
