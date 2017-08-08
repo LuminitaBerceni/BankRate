9b008c635f58dabf1d2b7e564c9b2a698f53f85d
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-08-08 06:11:59 +0300

    Add class Candidate

---
+++ JuniorMind7/CentralizedElections/Candidate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralizedElections
+{
+    public class Candidate
+    {
+        private string candidateName;
+        private int nrOfVotes;
+
+        public Candidate(string candidateName, int nrOfVotes)
+        {
+            this.candidateName = candidateName;
+            this.nrOfVotes = nrOfVotes;
+        }
+    }
+}
