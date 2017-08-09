6e98c36e3761d3c846adeb8350d4e43c16772754
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-08-09 05:49:04 +0300

    Add class PoolingStation

---
+++ JuniorMind7/CentralizedElections/PoolingStation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralizedElections
+{
+    class PoolingStation
+    {
+        private string poolingStationName;
+        private Candidate[] candidates;
+
+        public PoolingStation(string poolingStationName, Candidate[] candidates)
+        {
+            this.poolingStationName = poolingStationName;
+            this.candidates = candidates;
+        }
+    }
+}
