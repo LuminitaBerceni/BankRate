0c560f0681117e17709bd46edfc7b4674639f333
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-09-12 22:14:38 +0300

    Add Node class

---
+++ JuniorMind9/LinkedList/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Node<T>
+    {
+        private T data;
+        private Node<T> next;
+        private Node<T> previous;
+
+        public Node(T data)
+        {
+            this.data = data;
+        }
+    }
+}
