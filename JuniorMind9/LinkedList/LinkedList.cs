c6bf6d5695120e0d89fe018e62b43d881789e410
LuminitaBerceni <luminita_berceni@yahoo.com>
2017-10-04 07:32:41 +0300

    Implement LinkedList

--- JuniorMind9/LinkedList/LinkedList.cs
+++ JuniorMind9/LinkedList/LinkedList.cs
@@ -4,19 +4,149 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinkedList;
 
 namespace LinkedList
 {
     class LinkedList<T> : IEnumerable<T>
     {
+        private Node<T> sentinel;
+        private int count;
+
+        public LinkedList()
+        {
+            sentinel = new Node<T>(default(T));
+            sentinel.Next = sentinel;
+            sentinel.Previous = sentinel;
+            count = 0;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+
+        public void Add(T item)
+        {
+            AddLast(item);
+        }
+
+        public void AddFirst(T item)
+        {
+            Node<T> node = new Node<T>(item);
+            node.Previous = sentinel;
+            node.Next = sentinel.Next;
+            sentinel.Next.Previous = node;
+            sentinel.Next = node;
+
+            count++;
+        }
+
+        public void AddLast(T item)
+        {
+            Node<T> node = new Node<T>(item);
+            node.Next = sentinel;
+            node.Previous = sentinel.Previous;
+            sentinel.Previous.Next = node;
+            sentinel.Previous = node;
+
+            count++;
+        }
+
+        public bool Remove(T item)
+        {
+            for (Node<T> i = sentinel.Next; i != sentinel; i = i.Next)
+                if (i.Data.Equals(item))
+                {
+                    i.Previous.Next = i.Next;
+                    i.Next.Previous = i.Previous;
+                    count--;
+                    return true;
+                }
+            return false;
+        }
+
+        public void RemoveFirst()
+        {
+            if (sentinel.Next == sentinel)
+                throw new InvalidOperationException();
+            sentinel.Next = sentinel.Next.Next;
+            sentinel.Next.Previous = sentinel;
+            count--;
+        }
+
+        public void RemoveLast()
+        {
+            if (sentinel.Previous == sentinel)
+                throw new InvalidOperationException();
+            sentinel.Previous = sentinel.Previous.Previous;
+            sentinel.Previous.Next = sentinel;
+            count--;
+        }
+
+        public void Clear()
+        {
+            count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            for (Node<T> i = sentinel.Next; i != sentinel; i = i.Next)
+                if (i.Data.Equals(item))
+                    return true;
+            return false;
+        }
+
+        public Node<T> Find(T item)
+        {
+            Node<T> foundNode = null;
+            for (Node<T> i = sentinel.Next; i != sentinel; i = i.Next)
+                if (i.Data.Equals(item))
+                {
+                    foundNode = i;
+                    break;
+                }
+            return foundNode;
+        }
+
+        public void CopyTo(T[] destinationArray, int startIndex)
+        {
+            if (destinationArray == null)
+                throw new ArgumentNullException();
+            if (startIndex < 0)
+                throw new IndexOutOfRangeException();
+            if (count > destinationArray.Length - startIndex)
+                throw new ArgumentNullException();
+
+            int index = startIndex;
+            for (Node<T> i = sentinel.Next; i != sentinel; i = i.Next)
+            {
+                destinationArray[index] = i.Data;
+                index++;
+            }
+        }
+
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            Node<T> current = sentinel;
+            if (current != null)
+            {
+                do
+                {
+                    yield return current.Data;
+                    current = current.Next;
+                } while (current != sentinel);
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
         }
+
     }
 }
--- JuniorMind9/LinkedList/LinkedListTests.cs
+++ JuniorMind9/LinkedList/LinkedListTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace LinkedList
 {
@@ -7,8 +8,86 @@ namespace LinkedList
     public class LinkedListTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddTest()
         {
+            var linkedList = new LinkedList<int> { 2, 3 };
+            linkedList.Add(1);
+            Assert.AreEqual(3, linkedList.Count);
+        }
+
+        [TestMethod]
+        public void AddFirstTest()
+        {
+            var linkedList = new LinkedList<int> { 2, 3 };
+            linkedList.AddFirst(1);
+            Assert.IsTrue(linkedList.SequenceEqual(new LinkedList<int>() { 1, 2, 3}));
+        }
+
+        [TestMethod]
+        public void AddLastTest()
+        {
+            var linkedList = new LinkedList<int> { 2, 3 };
+            linkedList.AddLast(4);
+            Assert.IsTrue(linkedList.SequenceEqual(new LinkedList<int>() { 2, 3, 4 }));
+        }
+
+        [TestMethod]
+        public void RemoveFirstAppearanceTest()
+        {
+            var linkedList = new LinkedList<int> { 2, 1, 2, 3, 4 };
+            linkedList.Remove(2);
+            Assert.IsTrue(linkedList.SequenceEqual(new LinkedList<int>() { 1, 2, 3, 4 }));
+        }
+
+        [TestMethod]
+        public void RemoveFirstNodeTest()
+        {
+            var linkedList = new LinkedList<int> { 5, 6, 7, 8 };
+            linkedList.RemoveFirst();
+            Assert.IsTrue(linkedList.SequenceEqual(new LinkedList<int> { 6, 7, 8 }));
+        }
+
+        [TestMethod]
+        public void RemoveLastNodeTest()
+        {
+            var linkedList = new LinkedList<int> { 5, 6, 7, 8 };
+            linkedList.RemoveLast();
+            Assert.IsTrue(linkedList.SequenceEqual(new LinkedList<int> { 5, 6, 7 }));
+        }
+
+        [TestMethod]
+        public void ClearTest()
+        {
+            var linkedList = new LinkedList<int> { 5, 6, 7, 8 };
+            linkedList.Clear();
+            Assert.AreEqual(0, linkedList.Count);
+        }
+
+        [TestMethod]
+        public void ContainsTest()
+        {
+            var linkedList = new LinkedList<int> { 5, 6, 7, 8 };
+            Assert.IsTrue(linkedList.Contains(7));
+        }
+
+        [TestMethod]
+        public void FindNodeTest()
+        {
+            var linkedList = new LinkedList<int> { 1, 2, 3, 4 };
+            Node<int> node = linkedList.Find(3);
+            Assert.AreEqual(3, node.Data);
+            Assert.AreEqual(null, linkedList.Find(10));
+        }
+
+        [TestMethod]
+        public void CopyToTest()
+        {
+            var linkedList = new LinkedList<int> { 1, 6, 4 };
+            int[] newArray = new int[9];
+            int startIndex = 3;
+            linkedList.CopyTo(newArray, startIndex);
+
+            CollectionAssert.AreEqual(new int[] { 0, 0, 0, 1, 6, 4, 0, 0, 0 }, newArray);
         }
     }
 }
